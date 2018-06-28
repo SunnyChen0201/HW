57864299216cc4bfa6d5d427c35d65c0daf705ec
SUNNY <sunny1349@gmail.com>
2018-06-28 10:53:43 +0800

    1111111

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            marioPHOTO();
+        }
+
+        private void time_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void add_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            UserControl1 userControl = new UserControl1();
+            list.Children.Add(userControl);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            List<string> datas = new List<string>();
+            foreach (UserControl1 item in list.Children)
+            {
+                string uuu = "";
+                uuu += "|" + item.bbb + "|" + item.name;
+                datas.Add(uuu);
+
+            }
+        }
+
+        public void marioPHOTO()
+        {
+            BitmapImage mario = new BitmapImage();
+            mario.UriSource =new Uri (@"C:\mario.jpg", UriKind.Relative);
+
+            marioPTT.Source = mario;
+        }
+    }
+
+    
+
+}
---
+++ UserControl1.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// UserControl1.xaml 的互動邏輯
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+
+        public string Time
+        {
+            get
+            {
+                return bbb.Text;
+            }
+            set
+            {
+                bbb.Text = value.ToString();
+            }
+        }
+
+        public string NAMEEEE
+        {
+            get
+            {
+                return name.Text;
+            }
+            set
+            {
+                name.Text = value.ToString();
+            }
+        }
+    }
+
+
+}
+
+
+
+
